cf46f35365acb304d4df29b9d36369d63836f619
Charles Severance <csev@umich.edu>
2018-01-23 14:41:47 -0500

    Delete the tsugi

--- tsugi/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ( ! defined('COOKIE_SESSION') ) define('COOKIE_SESSION', true);
-require_once("config.php");
-use \Tsugi\Crypt\SecureCookie;
-session_start();
-session_unset();
-SecureCookie::delete();
-
-header('Location: '.$CFG->apphome);
